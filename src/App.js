85285a932e3bee547516040442c81b786d1c2a00
James Cornelius <jcorn2@umbc.edu>
2020-04-05 21:07:25 -0400

    Created hooks example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([
+    { text: "Learn JavaScript", done: false },
+    { text: "Learn React", done: false },
+    { text: 'Read more on hooks', done: false},
+    { text: "Play around in JSFiddle", done: true },
+    { text: "Build something awesome", done: true }
+  ]);
+  const [normalOrder, setNormalOrder] = useState(true);
+
+  const sort = () => {
+    setItems((prevItems) => {
+      const newItems = prevItems.slice().sort((a, b) => a.text < b.text);
+      if (normalOrder) {
+        newItems.reverse();
+      }
+      return newItems;
+    });
+    setNormalOrder((prevNormalOrder) => !prevNormalOrder);
+  };
+
+  useEffect(() => {
+    alert(`Your first task is ${items[0].text}`);
+  }, [items]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h2>Todos:</h2>
+      <ol>
+        {items.map(item => (
+          <li key={item.id}>
+            <label>
+              <input type="checkbox" disabled readOnly checked={item.done} /> 
+              <span className={item.done ? "done" : ""}>{item.text}</span>
+            </label>
+          </li>
+        ))}
+      </ol>
+      <button onClick={sort}>Sort</button>
+  </div>
   );
 }
 
